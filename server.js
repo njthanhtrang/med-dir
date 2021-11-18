cad7f1d4fdf1c81ad3dc74f879f0ef98e1993fc8
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-11-18 14:09:17 -0800

    sync sequelize models to mySQL db on server start

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const session = require("express-session");
 const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// import sequelize connection
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
